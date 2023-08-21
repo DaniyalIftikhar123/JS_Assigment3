db8de009b4ae6f11ac0621fb87bb02a0148985d8
DaniyalIftikhar123 <daniyal.iftikhar108@yahoo.com>
2023-08-21 20:46:43 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,70 @@
+//********Chapter 14 Qno1*******
+
+//var arr=[];
+
+//********Chapter 14 Qno2*******
+
+//var arr = new Array()
+
+//********Chapter 14 Qno3*******
+
+ //var strArr=["Daniyal","Iftikhar"]
+
+ //********Chapter 14 Qno4*******
+
+ //var numArr=[1,7,3,8,9]
+
+ //********Chapter 14 Qno5*******
+
+ //var boolArr=[true,false,true]
+
+ //********Chapter 14 Qno6*******
+
+ //var mixArr=["Daniyal",12,5,506,true,false,"Ahmed"]
+
+
+  //********Chapter 14 Qno7*******
+
+  // var docArr= ["SSC", "HSC", "BCS","BS", "BCOM", "MS", "M. Phil.", "PhD"]
+  // for(var i=1 ; i<docArr.length;i++){
+  //   document.write(i+") "+docArr[i]+"<br />")
+  // }
+
+
+  //********Chapter 14 Qno8*******
+
+  // var stuRec = ["Daniyal","Iftikhar","Ahmed"]
+  // var stuNum = [320,230,480]
+  // var totalMarks = 500
+
+  // document.write("Score of Daniyal is " +stuNum[0]+ " Percentage: " + (stuNum[0]/totalMarks)*100+"<br />")
+  // document.write("Score of Iftikhar is " +stuNum[1]+ " Percentage: " + (stuNum[1]/totalMarks)*100+"<br />")
+  // document.write("Score of Ahmed is " +stuNum[2]+ " Percentage: " + (stuNum[2]/totalMarks)*100+"<br />")
+
+  //********Chapter 14 Qno9*******
+
+//   var arr=["Red","Green","Blue","Orange","Yellow"]
+//   document.write(arr+"<br />");
+
+//   //a
+//   var addValue = prompt("What you like to do in beginning")
+//   arr.unshift(addValue)
+//   document.write(arr+"<br />")
+
+//   //b
+//   var addValueEnd = prompt("What you like to do in End")
+//   arr.push(addValueEnd)
+//   document.write(arr+"<br />")
+
+//   //c
+    
+//   arr.unshift("Indigo","Red")
+//   document.write(arr+"<br />")
+
+//   //d
+
+//   arr.shift()
+// document.write(arr+"<br />")
+//   //e
+//   arr.pop()
+// document.write(arr+"<br />")
